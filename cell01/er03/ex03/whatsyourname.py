fb819bbe2d5dba655784da993bc8697bafe01d4f
LongLee777 <nsukkha@student.42bangkok.com>
2025-03-11 10:53:29 +0700

    er03

---
+++ cell01/er03/ex03/whatsyourname.py
@@ -0,0 +1,6 @@
+# ขอให้ผู้ใช้ป้อนชื่อจริงและนามสกุล
+first_name = input("์Natthawat : ").strip()
+last_name = input("Sukkha : ").strip()
+
+# แสดงผลลัพธ์
+print(f"Well, pleased to meet you, {first_name} {last_name}.")
